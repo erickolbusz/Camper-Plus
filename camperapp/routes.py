fe27e7c9b71c25b607984669dc87475926be0a0d
erickolbusz <erickolbusz@gmail.com>
2017-10-31 00:30:50 -0400

    add schedule route

--- camperapp/routes.py
+++ camperapp/routes.py
@@ -7,3 +7,8 @@ from camperapp import app
 def index():
     """View displays the homepage"""
     return "<h1>Welcome To Camper+</h1>"
+
+@app.route('/schedule', methods=['GET','POST'])
+def schedule():
+    """View displays the schedule-making page"""
+    return "<h1>placeholder</h1>"
