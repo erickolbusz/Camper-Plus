abecf8d93d8cb51352c18c0795eb04578fbcbb1f
Kwokadyle <chriskwok52897@gmail.com>
2017-11-06 18:41:52 -0500

    Sign up Form

--- camperapp/Login.py
+++ camperapp/Login.py
@@ -1,4 +1,14 @@
+
 class LoginForm(Form):
     email = StringField('Email')
     password = PasswordField('Password')
     submit = SubmitField('Login')
+
+class SignupForm(Form):
+    first_name = StringField('First name')
+    last_name = StringField('Last name')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    # change this later to specify the possible roles the user can be
+    role = StringField('Role') 
+    submit = SubmitField('Sign up')
