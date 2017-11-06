173d2005af62104e9b669b0c4f928a5765dba440
Kwokadyle <chriskwok52897@gmail.com>
2017-11-06 18:51:49 -0500

    Login and Signup but does not function yet

--- camperapp/Login.py
+++ camperapp/Login.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-
+#does not function yet
 class LoginForm(Form):
     email = StringField('Email')
     password = PasswordField('Password')
