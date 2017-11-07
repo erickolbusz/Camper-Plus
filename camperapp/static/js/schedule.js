b5635f8e9585517a4d4f344446d7b4e5324200de
erickolbusz <erickolbusz@gmail.com>
2017-11-07 11:43:27 -0500

    setup

---
+++ camperapp/static/js/schedule.js
@@ -0,0 +1,9 @@
+//<i class="material-icons">create</i><button class="group1">Group 1</button><br>
+var groupDisplay = $("#groups");
+var groups = {};
+
+var addNewGroup = function(name, color, members) {
+}
+
+var editGroup = function(name, color, members) {
+}
