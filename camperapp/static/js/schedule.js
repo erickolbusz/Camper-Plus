33892d1d2f05b441d4a8d3765cc413f4becb93cf
erickolbusz <erickolbusz@gmail.com>
2017-11-07 13:39:01 -0500

    modals and js functions finished

--- camperapp/static/js/schedule.js
+++ camperapp/static/js/schedule.js
@@ -1,5 +1,5 @@
 //<i class="material-icons">create</i><button class="group1">Group 1</button><br>
-var groupDisplay = $("#groups");
+var groupDisplay = $("#groups")[0];
 var groups = [];
 
 var	addNewGroup = function(name, color, members) {
@@ -8,6 +8,16 @@ var	addNewGroup = function(name, color, members) {
 		color: color,
 		members: members
 	});
+	updateDisplay();
+}
+
+var updateDisplay = function() {
+	var s = "";
+	for (var i=0; i<groups.length; i++) {
+		var group = groups[i];
+		s += "<a class=\"modal-trigger\" href=\"#editGroup\"><i class=\"material-icons\">create</i></a><button class=\"group1\" id=\"group"+group.name+"1\">"+group.name+"</button><br>";
+	}
+	groupDisplay.innerHTML = s;
 }
 
 var editGroup = function(name, newColor, newMembers) {
@@ -18,4 +28,10 @@ var editGroup = function(name, newColor, newMembers) {
 			group.members = newMembers;
 		}
 	}
+	updateDisplay();
 }
+
+$(document).ready(function() {     
+  $('.modal').modal();
+  $('.trigger-modal').modal();
+});
