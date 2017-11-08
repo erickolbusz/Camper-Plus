1f23baec002cfbf51b6e2f15da3f88cdf35be50b
daniel <daniel.obeng@macaulay.cuny.edu>
2017-11-08 16:59:21 -0500

    adds ability to delete existing event

--- camperapp/static/js/calendar.js
+++ camperapp/static/js/calendar.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
                     window.console.log(calEvent)
                     $('#eventModal').modal('open');
                     $("#eventTitle").val(calEvent.title);
+                    $('#deleteEvent').removeClass("disabled")
                     $("#eventStartDate").val(calEvent.start.format('YYYY-MM-DD'))
                     $("#eventStartTime").val(calEvent.start.format('HH:mm:ss'))
                     $('#eventEndDate').val(calEvent.end.format('YYYY-MM-DD'))
@@ -40,6 +41,7 @@ $(document).ready(function() {
                 {
                     $('#eventModal').modal('open');
                     $("#eventTitle").val('');
+                    $('#deleteEvent').addClass("disabled")
                     $("#eventStartDate").val(start.format('YYYY-MM-DD'))
                     $("#eventStartTime").val(start.format('HH:mm:ss'))
                     $('#eventEndDate').val(end.format('YYYY-MM-DD'))
