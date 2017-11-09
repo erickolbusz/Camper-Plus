d941d98fdb0e03fc617178b31c202ba99ee7ea9f
Zhirayr Abrahamyan <zabraha000@citymail.cuny.edu>
2017-11-09 15:54:19 -0500

    updating- models.py

--- camperapp/models.py
+++ camperapp/models.py
@@ -1,38 +1,13 @@
 """Models in Camper APP"""
 
-<<<<<<< HEAD
 api_key = "AIzaSyDfnLfxNeG45N_tBtUutrB9K6-GYCtD7_I"
 
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
-"""
-will be lodified
-db = SQLAlchemy()
 
-class Parent(db.Model):
-  __tablename__ = 'Parent'
-  uid = db.Column(db.Integer, primary_key = True)
-  GroupName = db.Column(db.String(100))
-  GroupName = db.Column(db.String(100))
-  lastname = db.Column(db.String(100))
-  email = db.Column(db.String(120), unique=True)
-  pwdhash = db.Column(db.String(54))
 
-  def __init__(self, firstname, lastname, email, password):
-    self.firstname = firstname.title()
-    self.lastname = lastname.title()
-    self.email = email.lower()
-    self.set_password(password)
 
-  def set_password(self, password):
-    self.pwdhash = generate_password_hash(password)
-
-  def check_password(self, password):
-    return check_password_hash(self.pwdhash, password)
-"""
-
-=======
 
 class CampEvent(object):
     """Camp Event class representing an event on the calendar"""
