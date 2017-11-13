f8c9272da209b5247bdb51a389e56e97459141ee
daniel <daniel.obeng@macaulay.cuny.edu>
2017-11-13 13:36:31 -0500

    adds groups to schedule maker

--- camperapp/routes.py
+++ camperapp/routes.py
@@ -13,4 +13,10 @@ def index():
 @app.route('/schedule', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def schedule():
     """View displays the schedule-making page"""
-    return render_template("schedule.html")
+    # sample data, replace with db query
+    groups = [{"name": "Tigers", "color": "#4286f4", "groupid": 1},
+              {"name": "Falcons", "color": "#af2313", "groupid": 2},
+              {"name": "Bears", "color": "#191917", "groupid": 3},
+              {"name": "Lions", "color": "#FB8CB5", "groupid": 4},
+              {"name": "Jacks", "color": "#C9B09C", "groupid": 5}]
+    return render_template("schedule.html", groups=groups)
