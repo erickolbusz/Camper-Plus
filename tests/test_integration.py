8bac0bf8d74f3886a89f671128de155fecf26d1e
daniel <daniel.obeng@macaulay.cuny.edu>
2017-11-13 23:32:25 -0500

    more tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,6 +2,7 @@
 
 import unittest
 import camperapp
+import json
 
 
 class TestUrls(unittest.TestCase):
@@ -16,4 +17,16 @@ class TestUrls(unittest.TestCase):
     def test_calendar(self):
         """Test that the Calendar Page can be accessed"""
         response = self.app.get("/schedule")
+        self.assertTrue(response.status_code, 200)
+
+    def test_groups_on_schedule_page(self):
+        """Test that the groups passed to the schedule page are all displayed"""
+        json_data = {
+            'title': 'Test Event',
+            'start': '2017-8-8T12:00:00',
+            'end': '2017-8-8T12:00:00',
+            'group': '3'
+        }
+
+        response = self.app.post("/saveEvent", data=json.dumps(json_data), content_type='application/json')
         self.assertTrue(response.status_code, 200)
