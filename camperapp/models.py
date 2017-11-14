01a7f4fcfce6278fbc6951a3a5bce5f193da27d4
Zhirayr Abrahamyan <zabraha000@citymail.cuny.edu>
2017-11-14 02:23:16 -0500

    Changes models.py

--- camperapp/models.py
+++ camperapp/models.py
@@ -11,97 +11,28 @@ from werkzeug import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class CampEvent(db.Model):
-    __tablename__='events'
-    event_id = db.Column(db.Integer, primary_key = True)
-    event_title = db.Column(db.String())
-    event_start = db.Column(db.String())
-    events_end = db.Column(db.String())
+    __tablename__='event'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String())
+    start = db.Colum(db.DateTime())
+    end = db.Column(db.DateTime())
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
 
 
 
     """Camp Event class representing an event on the calendar"""
 
-    def __init__(self, event_title,event_start,events_end,group_id):
-        self.event_title = event_title.title()
-        self.event_start = event_start.title()
-        self.event_end = event_end.title()
-        self.eroup_id = group_id.title()
+    def __init__(self, title,start,end,group_id):
+        self.title = event_title.title()
+        self.start = event_start.title()
+        self.end = event_end.title()
+        self.group_id = group_id.title()
 
         """ it is not final """
     def __repr__(self):
         return '<CampEvent {}>'.format(self.title)
 
-    def load_all_appointments_in_range(cls, start, end):
-        """
-        Returns all Events between start date and end date as a list
-        param start: start date as a unix time string
-        param end: end date as a unix time string
-        return: a list of camp events
-        """
-        start_date = CampEvent.convert_from_unix_time_stamp(start)
-        end_date = CampEvent.convert_from_unix_time_stamp(end)
 
-        # TODO
-        events = CampEvent.query.all()
-        for event in events:
-    post.user_name = User.query.filter_by(id=post.id).first()
-return render_template('post.html', entries=posts)
-        # Query database and retrieve all appointments between start and end
-        # Store results in result
-        query_result = None(start_date, end_date)
-
-        calendar_events = []
-        for item in query_result:
-            event = CampEvent()
-            event.id = item.ID
-            event.title = item.Title
-            event.start_date = item.start_date
-            event.end_date = item.end_date
-            event.color = item.color
-            event.groups = item.group
-
-            calendar_events.append(event)
-
-        return calendar_events
-
-    # TODO
-    def get_camp_events(cls, start, end):
-        """
-        Returns all events between start date and end date as a json
-        param start: start date as a unix time string
-        param end: end date as a unix time string
-        return: a json containing camp events between start and end date
-        """
-        event_data = CampEvent.load_all_appointments_in_range(start, end)
-
-        #TODO parse event_data to JSON
-
-        return None
-
-    # TODO
-    def update_camp_event(cls, id, new_event_start, new_event_end):
-        """Update an existing event"""
-        # EventStart comes ISO 8601 format, eg:  "2000-01-10T10:00:00Z"
-        # - need to convert to DateTime
-
-        # query database and retrieve the event matching the ID
-        result = None
-
-        if result is not None:
-            # convert dates to database friendly date time
-            # Update the database with the new start and end date
-            pass
-
-        return None
-
-    # TODO
-    def convert_from_unix_time_stamp(cls, date):
-        """
-        Converts a unix time stamp to database friendly timestamp
-        """
-        pass
->>>>>>> camper/master
 
 class Group(db.Model):
     __tablename__='group'
