41804374952d94f2c2bd2535763e3c43adad916b
daniel <daniel.obeng@macaulay.cuny.edu>
2017-11-22 21:04:23 -0500

    creates parent registration template

--- camperapp/forms.py
+++ camperapp/forms.py
@@ -1,15 +1,26 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField, IntegerField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
-"""   this is not final """
+
 
 class LoginForm(Form):
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
-  submit = SubmitField("Sign in")
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+    submit = SubmitField("Sign in")
+
 
 class SignupFormManager(Form):
-  name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
-  submit = SubmitField('Sign up')
+    name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
+    submit = SubmitField('Sign up')
+
+
+class ChildEnrollmentForm(Form):
+    child_first_name = StringField('First name')
+    child_last_name = StringField('Last name')
+    child_birth_date = DateField('Birthday')
+    child_grade = IntegerField('Grade')
+    child_gender = SelectField('Gender', choices=['Male', 'Female'])
+    medical_notes = TextAreaField('Medical Notes')
+
