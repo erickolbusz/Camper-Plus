faaec6ca9564f45d48a9704dd9b851b37e0f3f3e
Obeng <daniel.obeng@macaulay.cuny.edu>
2017-11-27 17:49:07 -0500

    removes local db

--- camperapp/__init__.py
+++ camperapp/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://localhost/camper_plus"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://localhost/camper_plus"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "0QZSIXW7U50133JZuZ6gpkwZ9yYCXY"
 heroku = Heroku(app)
