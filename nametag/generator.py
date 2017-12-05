04ae22456915e90e86b240e11acf26d926710ef4
erickolbusz <erickolbusz@gmail.com>
2017-12-05 14:56:14 -0500

    robustify cwd and add opensans font

--- nametag/generator.py
+++ nametag/generator.py
@@ -1,9 +1,11 @@
 import PIL
+import os
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
+cwd = os.path.dirname(os.path.realpath(__file__))
 
-font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 25)
+font = ImageFont.truetype(cwd+"/OpenSans-Regular.ttf", 25)
 img = Image.new("RGBA", (200,200), (120,20,20))
 draw = ImageDraw.Draw(img)
 draw.text((0,0), "abcdefghijklmnopQRSTUVWXYZ", (255,255,0), font=font)
